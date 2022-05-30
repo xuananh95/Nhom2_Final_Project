7b38f8942b14a0a1cd35e27e598359f878e771f5
heocon1207bee <duckhanh.hoang.1481@gmail.com>
2022-05-30 17:10:35 +0700

    edit css
    chỉnh lại giao diện

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './styles.css'
+<<<<<<< Updated upstream
 import { useState } from 'react'
 
 
 const Header = () => {
   const [searchDisplay, setSearchDisplay] = useState(false)
+=======
+import { toast } from 'react-toastify';
+import {BsSearch} from 'react-icons/bs'
+import {MdExpandMore} from 'react-icons/md'
+import {IoCloseSharp} from 'react-icons/io5'
+
+
+const Header = () => {
+  const [searchDisplay, setSearchDisplay] = useState(false);
+
+  let activeStyle = {
+    color: "rgb(255, 107, 77)",
+  }
+  const [currentUser, setCurrentUser] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const current = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : null;
+    setCurrentUser(current);
+  }, [location]);
+
+  const handleLogout = () => {
+    localStorage.removeItem('currentUser');
+    navigate('../sign-in', {replace: true});
+    toast.success('Log out successful!');
+  }
+>>>>>>> Stashed changes
 
   return (
     <div className='header'>
@@ -13,24 +42,47 @@ const Header = () => {
           <Link to='/'>tickets</Link>
         </div>
         <div className='header-menu header-units'>
-          Menu List
+          Menu List <MdExpandMore/>
           <div className='dropdown-menu'>
             <Link to=''>About us</Link>
             <Link to=''>Cinema list</Link>
           </div>
         </div>
         <div className='header-search header-units'>
-          {!searchDisplay ? <div onClick={() => setSearchDisplay(!searchDisplay)}>(Search-Icon)</div> : (
+          {!searchDisplay ? <div onClick={() => setSearchDisplay(!searchDisplay)}><BsSearch/></div> : (
           <form>
             <input type='search' placeholder='Search'></input>
-            <button onClick={() => setSearchDisplay(!searchDisplay)}>CLOSE</button>
+            <button onClick={() => setSearchDisplay(!searchDisplay)}><IoCloseSharp/></button>
           </form>)}
         </div>
         <div style={{width: '100%'}}> </div>
+<<<<<<< Updated upstream
         <div className='header-account header-units'>
           <Link to='' className='sign-in-link account-link'>Sign In</Link>
           <Link to='' className='sign-up-link account-link'>Sign Up</Link>
         </div>
+=======
+
+        {currentUser ? (
+          <div className="header-account header-units">
+            {(currentUser.isAdmin && (
+                <NavLink style={({isActive}) => 
+                  isActive ? activeStyle : undefined} to="/admin">Administrator
+                </NavLink>
+            ))}
+            <NavLink style={({isActive}) => 
+                  isActive ? activeStyle : undefined} to="/user">Xin chào {currentUser.username}!
+            </NavLink>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+      ) : ( 
+        <div className="header-account header-units">
+              <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to="/sign-up" className='account-link'>Sign up</NavLink>
+              <NavLink style={({ isActive }) => isActive ? activeStyle : undefined} to="/sign-in" className='account-link sign-up-link'>Sign in</NavLink>
+        </div>      
+      ) 
+      }
+>>>>>>> Stashed changes
     </div>
   )
 }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import Footer from '../Footer'
 import Header from '../Header'
+import styled from 'styled-components'
 
 const Layout = ({children}) => {
   return (
+<<<<<<< Updated upstream
     <div>
         <Header />
         {children}
         <Footer />
     </div>
+=======
+    <SLayout>
+      <Header />
+      {children}
+      <Footer />
+    </SLayout>
+>>>>>>> Stashed changes
   )
 }
 
 export default Layout
+
+const SLayout = styled.div`
+  position: relative;
+  min-height: 100vh;
+  height: fit-content;
+`
